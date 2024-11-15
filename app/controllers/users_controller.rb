545eb4c0868cc752bc3aa7c7d3392515d9671419
kamakurayuma <30235ud@gmail.com>
2024-11-15 18:09:06 +0900

    フラッシュメッセージを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,15 +8,17 @@ class UsersController < ApplicationController
     def create
       @user = User.new(user_params)
       if @user.save
-        redirect_to root_path
+        auto_login(@user)
+        redirect_to root_path, success: '登録が完了しました'
       else
-        render :new
+        flash.now[:danger] = 'ユーザー登録に失敗しました'
+        render :new, status: :unprocessable_entity
       end
     end
   
     private
   
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:user_name, :email, :password, :password_confirmation)
     end
 end
