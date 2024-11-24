ed2dac141a67fe8781f3a564c4606b38e7110ca9
kamakurayuma <30235ud@gmail.com>
2024-11-24 19:20:41 +0900

    add:件数表示

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,14 +2,17 @@ class StaticPagesController < ApplicationController
     skip_before_action :require_login, only: %i[top]
   
     def top
-        # params[:q] が nil でない場合にのみ処理を行うようにする
-        search_params = params[:q] || {}
-        query_cont = search_params[:query_cont]
-      
-        @q = Board.ransack(
-          camera_make_or_camera_model_cont: query_cont
-        )
-        @boards = @q.result(distinct: true).includes(:user, :camera).order(created_at: :desc).page(params[:page]).per(30)
-      end
-      
-end
+      search_params = params[:q] || {}
+      query_cont = search_params[:query_cont]
+  
+      # Ransackで検索結果を取得
+      @q = Board.ransack(camera_make_or_camera_model_cont: query_cont)
+  
+      # 検索結果の総件数を取得
+      @total_boards_count = @q.result(distinct: true).count
+  
+      # ページネーション用のデータを取得
+      @boards = @q.result(distinct: true).includes(:user, :camera).order(created_at: :desc).page(params[:page]).per(30)
+    end
+  end
+  
