98d357ae26ddefe07a2739317c8afb201da0c979
kamakurayuma <30235ud@gmail.com>
2025-01-08 13:05:40 +0900

    CItestのチェック14

--- config/initializers/check_jawsdb_url.rb
+++ config/initializers/check_jawsdb_url.rb
@@ -1,3 +1,5 @@
-if ENV["JAWSDB_URL"].blank?
-    raise "JAWSDB_URL is not set. Please set the JAWSDB_URL environment variable."
+return if Rails.env.test?
+
+if ENV['JAWSDB_URL'].nil?
+  raise "JAWSDB_URL is not set. Please set the JAWSDB_URL environment variable."
 end
