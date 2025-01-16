1ba1461a79bdf88ec98a8788d9b38852e40134f0
kamakurayuma <30235ud@gmail.com>
2025-01-16 12:29:20 +0900

    デプロイエラー

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'                        # 使うプロバイダを指定
+    config.fog_provider = 'fog/aws'  # または適切なクラウドストレージプロバイダ
     config.fog_credentials = {
-      provider: 'AWS',                                      # プロバイダとしてAWSを指定
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],          # AWSアクセスキー
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],  # AWSシークレットキー
-      region: 'us-west-1'                                   # 使用するリージョン
+      provider:              'AWS',              # 必要に応じて他のプロバイダ
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region:                'us-east-1',
     }
-    config.fog_directory  = ENV['AWS_BUCKET']               # 使用するS3バケット
-    config.fog_public     = true                             # 公開設定
+    config.fog_directory  = ENV['AWS_S3_BUCKET_NAME']
+    config.fog_public     = false  # または必要に応じて設定
 end
   
