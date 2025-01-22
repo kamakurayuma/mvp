341b7a6b25a2700ff76896c135759528ebb43e17
kamakurayuma <30235ud@gmail.com>
2025-01-22 14:37:02 +0900

    s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'  # S3を使うための指定
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # Herokuの環境変数で設定されているか
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # Herokuの環境変数で設定されているか
-      region: ENV['AWS_REGION'], # 必要に応じて設定
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_REGION'],
     }
-    config.fog_directory = ENV['AWS_BUCKET']  
-    config.fog_public = true  
-  end
+    config.fog_directory = ENV['AWS_BUCKET']
+    config.fog_public = true
+end
   
