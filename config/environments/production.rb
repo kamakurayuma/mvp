4433a26d08c12fedafab238b401b91de1ea8a6e2
kamakurayuma <30235ud@gmail.com>
2025-01-30 15:19:46 +0900

    reset_passwordのメール修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "oldcam1700.jp", protocol: "https" }
+  config.action_mailer.default_url_options = { host: "www.oldcam1700.jp", protocol: "https" }
   config.action_mailer.raise_delivery_errors = true
   # Add SMTP settings for Action Mailer
   config.action_mailer.delivery_method = :smtp
